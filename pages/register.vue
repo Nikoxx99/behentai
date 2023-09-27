d069da484871f6ec4b726c6c695db9f3479352f1
Nicolas Echeverry <nikoxx9900@gmail.com>
2023-09-27 11:30:48 -0500

    Initial prisma config and Vuetify

---
+++ pages/register.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Register page</h1>
+    <p>{{ allUsers }}</p>
+  </div>
+</template>
+<script setup>
+  definePageMeta({
+    layout: "behentai",
+  });
+  const prisma = usePrisma()
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+</script>
